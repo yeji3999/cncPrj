f40a550197179a312a7316ce3fe8140880110c0e
18.04_ubuntu <yeji3999@naver.com>
2021-05-27 22:49:06 -0700

    bar chart add  datalabels

--- src/components/TopPanels/BarChart.vue
+++ src/components/TopPanels/BarChart.vue
@@ -6,13 +6,15 @@
 
 <script>
 import Chart from 'chart.js'
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 export default {
   name: 'PlanetChart',
   mounted() {
     const ctx = document.getElementById('planet-chart');
     Chart.defaults.global.defaultFontColor = '#D3D3D3';
-    new Chart(ctx, this.$store.state.ctLineData);
+    Chart.plugins.register(ChartDataLabels);
+    new Chart(ctx, this.$store.state.ctLineData); 
   }
 }
 </script>
