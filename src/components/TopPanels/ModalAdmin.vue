dfb54006fa80cb602d1dcd8c4b1604c2f1b54dba
yeji <yj.yoon6114@hncorp.world>
2021-07-05 15:06:41 +0900

    modal admin

--- src/components/TopPanels/ModalAdmin.vue
+++ src/components/TopPanels/ModalAdmin.vue
@@ -7,12 +7,12 @@
     </h3>
     <div slot="body">
         <div id="predicModelStatus" style="width:100%">
-            <div style=" display:inline-block; width:50%">
-            <div class="modalSubTitle">예측 모델 현황</div>
+          <div class="modalSubTitle">예측 모델 현황</div>
+            <div style=" display:inline-block; width:50%; position:relative; bottom: 75px">  
             <span class="modalsubText">현재 {{modelInUse}} 모델 사용 중 입니다. </span>
             </div>
-            <div style=" display:inline-block">
-                <div>chartArea</div>
+            <div style=" display:inline-block">   
+              <iframe v-bind:src="realTimeIframe" width="100%" height="90%" frameborder="1"></iframe> 
             </div>
         </div>
 
@@ -60,7 +60,8 @@ export default({
       modelInUse: "Model1",
       Model1predicionRate: "0.11",
       Model2predicionRate: "0.123",
-      changeModelValues: ""
+      changeModelValues: "",
+      realTimeIframe: "http://9.8.100.152:8083/realTime.html",
     }
     
   },
@@ -93,12 +94,15 @@ export default({
 .modalSubTitle{
     font-size: 20px;
     font-weight: bold;
+    color: rgb(221, 221, 221)
 }
 .ModelChangeTitle{
-    margin-left:10px
+    margin-left:10px;
+    color: rgb(221, 221, 221)
 }
 .ModelPredicRate{
-   margin-left: 20px 
+   margin-left: 20px;
+   color: rgb(221, 221, 221)
 }
 #modelChangeSubmit{
     background: #38e09a;
@@ -107,5 +111,8 @@ export default({
     height: 30px;
     width: 90px;
     border-radius: 5px;
+}
+.modalsubText{
+  color: rgb(221, 221, 221)
 }
 </style>
