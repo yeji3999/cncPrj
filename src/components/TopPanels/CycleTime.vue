2662b5875234b51e6d03d96d4d79e7ae12bd45fb
cybang <1027alsl@naver.com>
2021-07-07 14:29:08 +0900

    remove log

--- src/components/TopPanels/CycleTime.vue
+++ src/components/TopPanels/CycleTime.vue
@@ -15,7 +15,6 @@ export default {
   },
   sockets: {
     cycleTimeMean: function(miliTime) {
-      console.log(miliTime, " whwhwhwhwhy")
       let avgTime = this.numToMS(miliTime);
       this.cycleTimeVal = avgTime;
     },
