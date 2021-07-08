cefa0f4ac0542c993653455c98e4359f20f2cf34
yeji <yj.yoon6114@hncorp.world>
2021-07-08 14:03:04 +0900

    layout menu

--- src/components/HeaderMenu.vue
+++ src/components/HeaderMenu.vue
@@ -4,12 +4,15 @@
         <button @click="modalAdmin" id="modelChange" v-if="isAdmin" ><i class="fa fa-cog fa-2x" aria-hidden="true"></i></button>
 
         <div id="layoutBtn" :style="{display:layoutshow}">
-        <!-- <button style="margin-left:5px"><img src="../../src/assets/layout1.png" style="width:30px" alt=""></button> -->
+          <div> <span style="margin-left:5px; font-weight:600">Layout</span>
+  <button style="float: right; color:#595959; margin:0px" @click="closeLayoutMenu"><i class="fa fa-times"></i></button>
+  </div>
+  <div id="layoutBtnArea">
         <button @click="layout1" style="margin-left:5px"><img :src="layout1Icon" style="width:30px" alt=""></button>
-
         <button @click="layout2"><img :src="layout2Icon" style="width:30px" alt=""></button>
         <button @click="layout3" style="margin-right:4px"><img :src="layout3Icon" style="width:30px" alt=""></button>
         </div> 
+        </div>
 
     </div>
 </template>
@@ -31,6 +34,9 @@ created() {
     }
   },
 methods:{
+  closeLayoutMenu(){
+    this.layoutshow = "none"
+  },
   layout1(){
     this.layout1Icon = require("../../src/assets/click_layout1.png")
     this.layout2Icon = require("../../src/assets/layout2.png")  
@@ -77,6 +83,9 @@ methods:{
 
 
 <style scoped>
+#layoutBtnArea{
+  margin-top: 10px
+}
 #layoutBtn{
   background: white;
   border-radius: 5px;
